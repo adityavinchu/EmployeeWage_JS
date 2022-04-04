e7f130be9cb382e8dc10cbf58bf69ed9f586c58e
aditya <vinchuaditya@gmail.com>
2022-04-04 18:26:20 +0530

    [Aditya] added: storing Daily Wage in a map

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,4 +1,4 @@
-//UC6
+//UC7
 
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
@@ -9,11 +9,10 @@ const WAGE_PER_HOUR=20;
 const MAX_HRS_IN_MONTH=160;
 const NUM_OF_WORKING_DAYS=20;
 
-let empHrs=0;
+
 let totalEmpHrs=0;
 let totalWorkingDays=0;
 
-let empDailyWageArray = new Array();
 
 function getWorkingHours(EmpCheck)
 {
@@ -30,6 +29,9 @@ function getWorkingHours(EmpCheck)
     }
 }
 
+let empDailyWageArray = new Array();
+let empDailyWageMap = new Map(); 
+
 function calculateDailyWage(empHrs)
 {
     return empHrs * WAGE_PER_HOUR;
@@ -39,10 +41,18 @@ while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
 {
     totalWorkingDays++;
     let EmpCheck=Math.floor(Math.random()*10)%3;
-    totalEmpHrs = getWorkingHours(EmpCheck);
+    let empHrs=getWorkingHours(EmpCheck);
+    //console.log(EmpCheck);
     totalEmpHrs += empHrs;
     empDailyWageArray.push(calculateDailyWage(empHrs));
+    empDailyWageMap.set(totalWorkingDays, calculateDailyWage(empHrs));
 }
 
-let empWage = calculateDailyWage(totalEmpHrs);
-console.log("Total days: "+totalWorkingDays+"  total Hrs: " +totalEmpHrs+"  Employee Wage: "+empWage);
+console.log(empDailyWageMap);
+function totalWages(totalWage,dailyWage)
+{
+    return totalWage+dailyWage;
+}
+
+//let empWage = calculateDailyWage(totalEmpHrs);
+console.log("Total : "+Array.from(empDailyWageMap.values()).reduce(totalWages,0));
