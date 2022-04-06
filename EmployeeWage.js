f0084c478bff0b4c99d2d306eff6c659ee67ca49
aditya <vinchuaditya@gmail.com>
2022-04-06 19:22:53 +0530

    Arrow function added

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,4 +1,4 @@
-//UC8
+//UC9
 
 const IS_PART_TIME=1;
 const IS_FULL_TIME=2;
@@ -32,6 +32,8 @@ function getWorkingHours(EmpCheck)
 let empDailyWageArray = new Array();
 let empDailyWageMap = new Map(); 
 
+let empDailyhrsmap =new Map();
+
 function calculateDailyWage(empHrs)
 {
     return empHrs * WAGE_PER_HOUR;
@@ -46,6 +48,7 @@ while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
     totalEmpHrs += empHrs;
     empDailyWageArray.push(calculateDailyWage(empHrs));
     empDailyWageMap.set(totalWorkingDays, calculateDailyWage(empHrs));
+    empDailyhrsmap.set(empHrs);
 }
 
 console.log(empDailyWageMap);
@@ -53,6 +56,25 @@ function totalWages(totalWage,dailyWage)
 {
     return totalWage+dailyWage;
 }
+let empWage = calculateDailyWage(totalEmpHrs);
 
-//let empWage = calculateDailyWage(totalEmpHrs);
+const findTotal=(totalWage, dailyWage)=>{return totalWage + dailyWage ;}
+
+totalEmpHrs=Array.from(empDailyhrsmap.values()).reduce(findTotal,0);
+let totalSalary=empDailyWageArray.filter(dailyWage=>dailyWage>0).reduce(findTotal,0);
+
 console.log("Total : "+Array.from(empDailyWageMap.values()).reduce(totalWages,0));
+
+let nonWorkingDays= new Array();
+let partTimeWorkingDays= new Array();
+let fullTimeWorkingDays= new Array();
+
+empDailyhrsmap.forEach((value,key,map)=>{
+    if(value==8)fullTimeWorkingDays,push(key);
+    else if (value==4) partTimeWorkingDays.push(key);
+    else nonWorkingDays.push(key);
+});
+
+console.log("Full working days:"+fullTimeWorkingDays);
+console.log("Part working days:"+partTimeWorkingDays);
+console.log("Non Working Days:" +nonWorkingDays);
